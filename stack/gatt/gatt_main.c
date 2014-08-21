b2de0b6d4fdae534d1ca449d3393343f709734be
Subramanian Srinivasan <subrsrin@codeaurora.org>
2014-08-21 16:46:04 -0700

    Dequeues direct connection request during cancel conn operation
    When cancel connection request for a device is sent
    from an app and if the current pending connection
    request's BD address does not match with this device,
    the entries of the connection request pending queue
    are also checked. If BD address match occurs with an
    entry in the connection request queue, the entry is
    removed from the queue.
    
    CRs-fixed: 715606
    
    Change-Id: I1bf50a424d86ac53a5201fff742c822f4c8d1c0b

--- stack/gatt/gatt_main.c
+++ stack/gatt/gatt_main.c
@@ -228,6 +228,11 @@ BOOLEAN gatt_disconnect (tGATT_TCB *p_tcb)
                 else
                 {
                     ret = L2CA_CancelBleConnectReq (p_tcb->peer_bda);
+
+                    if(!ret)
+                    {
+                        gatt_set_ch_state(p_tcb, GATT_CH_CLOSE);
+                    }
                 }
                 gatt_set_ch_state(p_tcb, GATT_CH_CLOSING);
             }
