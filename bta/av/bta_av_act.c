975803632f9e87c78957676c9d2481741b3637b7
JivakDhadse <jdhads@codeaurora.org>
2015-12-29 19:37:17 +0530

    AVRCP1.6: Reset Cover Art supported feature bit.
    Reset Cover Art supported feature bit in SDP record if
    fallback to lower version.
    CRs-Fixed: 956637
    Change-Id: I47bdc5dcbc777ba3afb90be1d52b0f6a152dde7b

--- bta/av/bta_av_act.c
+++ bta/av/bta_av_act.c
@@ -2001,11 +2001,17 @@ tBTA_AV_FEAT bta_av_check_peer_features (UINT16 service_uuid)
                         peer_features |= (BTA_AV_FEAT_BROWSE);
                         APPL_TRACE_DEBUG("peer supports browsing");
                     }
+                    if (categories & AVRC_SUPF_CT_COVER_ART_GET_IMAGE & AVRC_SUPF_CT_COVER_ART_GET_THUMBNAIL)
+                    {
+                        peer_features |=  BTA_AV_FEAT_CA;
+                        APPL_TRACE_DEBUG("peer supports cover art");
+                    }
                 }
             }
             property_get("persist.service.bt.a2dp.sink", a2dp_role, "false");
             if (!strncmp("false", a2dp_role, 5)) {
-                if ((peer_rc_version >= AVRC_REV_1_4) && (peer_features & BTA_AV_FEAT_BROWSE))
+                if ((peer_rc_version >= AVRC_REV_1_4) &&
+                        ((peer_features & BTA_AV_FEAT_BROWSE) || (peer_features & BTA_AV_FEAT_CA)))
                 {
                     BOOLEAN ret = FALSE;
                     APPL_TRACE_DEBUG("peer version to update: 0x%x", peer_rc_version);
