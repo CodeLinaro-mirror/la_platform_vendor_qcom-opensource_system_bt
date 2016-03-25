codeaurora-unoffical/platform-external-bluetooth-bluedroid

Bug 951530 - [b2g-bluetooth][HFP] Configure bluedroid for B2G.
Change-Id: I2c600052d158193d694505f8806ed07f12f2b78d

bta/ag/bta_ag_cfg.c
@@ -23,6 +23,7 @@
  *
  ******************************************************************************/
 
+#include "../../../../../gecko/dom/bluetooth/bluedroid/b2g_bdroid_buildcfg.h"
 #include "gki.h"
 #include "bta_api.h"
 #include "bta_ag_api.h"

btif/src/btif_hf.c
@@ -25,6 +25,7 @@
  *
  ***********************************************************************************/
 
+#include "../../../../../gecko/dom/bluetooth/bluedroid/b2g_bdroid_buildcfg.h"
 #include <hardware/bluetooth.h>
 #include <hardware/bt_hf.h>
 #include <stdlib.h>

CodeLinaro-mirror/la_platform_vendor_qcom-opensource_system_bt

bta/ag/bta_ag_cfg.c
@@ -23,6 +23,7 @@
  *
  ******************************************************************************/
 
+#include "../../../../../gecko/dom/bluetooth2/bluedroid/b2g_bdroid_buildcfg.h"
 #include "gki.h"
 #include "bta_api.h"
 #include "bta_ag_api.h"

btif/src/btif_hf.c
@@ -25,6 +25,7 @@
  *
  ***********************************************************************************/
 
+#include "../../../../../gecko/dom/bluetooth2/bluedroid/b2g_bdroid_buildcfg.h"
 #include <hardware/bluetooth.h>
 #include <hardware/bt_hf.h>
 #include <stdlib.h>