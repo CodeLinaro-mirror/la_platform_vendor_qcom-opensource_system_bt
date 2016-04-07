f98e197cd05cdf21e79560f44227f4d02859deba
Satish Kodishala <skodisha@codeaurora.org>
2016-04-07 15:56:34 +0530

    Don't unset the HF indicators and S4 Settings for PTS
    PTS Test Case HFP_AG_SLC_BV_10_I failed due to PTS HFP version is 1.5.
    If there is HFP Version less than 1.7 we are unsetting the HF indicators
    and S4 settings in BRSF.So,regardless of PTS HFP version we should not
    unset the HF indicators and S4 settings in BRSF.
    Also for HSP Profile some PTS Test cases failed because when there is
    HSP connection we are checking for callsetup indicator which were not
    present for HSP profile. So, we should check for HFP connection as well
    with the call setup Indicator.
    
    CRs-Fixed: 1000512
    Change-Id: I61239a187c2f85b759eb3029c47e6c2fe341c16e

--- bta/ag/bta_ag_cmd.c
+++ bta/ag/bta_ag_cmd.c
@@ -36,6 +36,7 @@
 #include "gki.h"
 #include "port_api.h"
 #include "utl.h"
+#include <cutils/properties.h>
 
 
 /*****************************************************************************
@@ -1029,6 +1030,7 @@ void bta_ag_at_hfp_cback(tBTA_AG_SCB *p_scb, UINT16 cmd, UINT8 arg_type,
     UINT32          i, ind_id;
     UINT32          bia_masked_out;
     tBTA_AG_FEAT  features;
+    char value[PROPERTY_VALUE_MAX];
 #if (BTM_WBS_INCLUDED == TRUE )
     tBTA_AG_PEER_CODEC  codec_type, codec_sent;
 #endif
@@ -1260,8 +1262,13 @@ void bta_ag_at_hfp_cback(tBTA_AG_SCB *p_scb, UINT16 cmd, UINT8 arg_type,
             /* if the devices does not support HFP 1.7, report DUT's HFP version as 1.6 */
             if (p_scb->peer_version < HFP_VERSION_1_7)
             {
-                features = features & ~(BTA_AG_FEAT_HFIND | BTA_AG_FEAT_S4);
-            }
+                /* For PTS keep flags as is. */
+                if (property_get("bt.pts.certification", value, "false") &&
+                    strcmp(value, "true") != 0)
+                {
+                    features = features & ~(BTA_AG_FEAT_HFIND | BTA_AG_FEAT_S4);
+                }
+             }
             /* send BRSF, send OK */
             bta_ag_send_result(p_scb, BTA_AG_RES_BRSF, NULL,
                                (INT16) features);
@@ -2032,6 +2039,13 @@ void bta_ag_send_bcs(tBTA_AG_SCB *p_scb, tBTA_AG_DATA *p_data)
 void bta_ag_send_ring(tBTA_AG_SCB *p_scb, tBTA_AG_DATA *p_data)
 {
     UNUSED(p_data);
+
+    if ((p_scb->conn_service == BTA_AG_HFP) &&
+         p_scb->callsetup_ind != BTA_AG_CALLSETUP_INCOMING)
+    {
+        APPL_TRACE_DEBUG("don't send the ring since there is no MT call setup");
+        return;
+    }
 
 #if defined(BTA_AG_MULTI_RESULT_INCLUDED) && (BTA_AG_MULTI_RESULT_INCLUDED == TRUE)
     tBTA_AG_MULTI_RESULT_CB m_res_cb;
