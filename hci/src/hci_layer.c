1c75f938142a36cd813053e0d36c4214faa5281a
Sharvil Nanavati <sharvil@google.com>
2016-06-20 19:16:12 -0700

    DO NOT MERGE Fix potential DoS caused by delivering signal to BT process
    Bug: 28885210
    
     Conflicts:
    	btif/co/bta_hh_co.c
    	btif/src/btif_core.c
    Merge conflict resolution of ag/1161415 (referencing ag/1164670)
    - Directly into mnc-mr2-release
    
    Change-Id: I63866d894bfca47464d6e42e3fb0357c4f94d360
    (cherry picked from commit 472271b153c5dc53c28beac55480a8d8434b2d5c)
    
    Conflicts:
    	btif/src/btif_hl.c

--- hci/src/hci_layer.c
+++ hci/src/hci_layer.c
@@ -20,6 +20,7 @@
 
 #include <assert.h>
 #include <cutils/properties.h>
+#include <errno.h>
 #include <string.h>
 #include <signal.h>
 #include <string.h>
@@ -523,7 +524,7 @@ static void command_timed_out(UNUSED_ATTR void *context) {
 
   LOG_ERROR("%s restarting the bluetooth process.", __func__);
   ssr_cleanup(0x22);//SSR reasno 0x22 = CMD TO
-  usleep(20000);
+  TEMP_FAILURE_RETRY(usleep(20000));
   //Reset SOC status to trigger hciattach service
   if (property_set("bluetooth.status", "off") < 0) {
      LOG_ERROR("hci_cmd_timeout: Error resetting SOC status\n ");
