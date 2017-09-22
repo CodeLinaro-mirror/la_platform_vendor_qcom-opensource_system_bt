f2e86f26a320d9775c6a009d22760cc90fe7a83c
Satheesh Kumar Pallemoni <palsat@codeaurora.org>
2017-09-22 19:12:42 +0530

    Audio latency indication for particular devices(1/2)
    Identification of the devices which has audio latency.
    Indicating this latency to the audio module.
    
    CRs-Fixed: 2131364
    Change-Id: I03107f81b261efd62ec0968da2002967ac073230

--- audio_a2dp_hw/bthost_ipc.h
+++ audio_a2dp_hw/bthost_ipc.h
@@ -167,5 +167,6 @@ typedef struct {
     void (*clear_a2dpsuspend_flag)(void);
     void*(*audio_get_next_codec_config)(uint8_t idx, audio_format_t *codec_type);
     int (*audio_check_a2dp_ready)(void);
+    int (*audio_get_addl_latency)(void);
 } bt_host_ipc_interface_t;
 #endif
