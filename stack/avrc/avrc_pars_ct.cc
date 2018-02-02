163057223ec332eca01b6d61c7fbe0176c039e8d
Ajay Panicker <apanicke@google.com>
2018-02-02 00:56:43 -0800

    AVRCP: Check number of text attributes in response
    Test: Build
    Bug: 71603315
    Change-Id: Ieda5e410057062533ae09bd977bfe7f758a55140
    (cherry picked from commit 658fd1b7c4ee959e42c20a2f1cfb7d895f94f6d2)
    (cherry picked from commit 907988f08315bc5ea2f00ae7919667ca45d4502f)

--- stack/avrc/avrc_pars_ct.cc
+++ stack/avrc/avrc_pars_ct.cc
@@ -529,6 +529,12 @@ static tAVRC_STS avrc_ctrl_pars_vendor_rsp(tAVRC_MSG_VENDOR* p_msg,
       BE_STREAM_TO_UINT8(p_result->list_app_attr.num_attr, p);
       AVRC_TRACE_DEBUG("%s attr count = %d ", __func__,
                        p_result->list_app_attr.num_attr);
+
+      if (p_result->list_app_attr.num_attr > AVRC_MAX_APP_ATTR_SIZE) {
+        android_errorWriteLog(0x534e4554, "63146237");
+        p_result->list_app_attr.num_attr = AVRC_MAX_APP_ATTR_SIZE;
+      }
+
       for (int xx = 0; xx < p_result->list_app_attr.num_attr; xx++) {
         BE_STREAM_TO_UINT8(p_result->list_app_attr.attrs[xx], p);
       }
