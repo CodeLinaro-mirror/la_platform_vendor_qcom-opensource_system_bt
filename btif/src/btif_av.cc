f0dd1167251b375c337977aca48c83a9eab921ad
Sravan Voleti <sravankumar@codeaurora.org>
2018-04-17 16:26:20 +0530

    AVRCP_CTRL: Fix abnormal behavior with IOS devices
    Use case:
    1. Pair and connect DUT with remote.
    
    Result:
    connection with IOS devices being disconnected
    within two seconds after connection is established.
    
    Root cause:
    Dut disconnecting AVRCP Connection with devices
    where AVRCP connection comes first bcz of invalid
    condition.
    
    Fix:
    Add proper check with remote peer, before initiate
    disconnect.
    
    CRs-Fixed: 2224120
    Change-Id: I30b49a9b7055c3204349c82804f64aea33108cf7

--- btif/src/btif_av.cc
+++ btif/src/btif_av.cc
@@ -356,7 +356,7 @@ static void btif_initiate_av_open_timer_timeout(UNUSED_ATTR void* data) {
           /*
            * Check if A2DP conneciton is with same device
            */
-          if(btif_av_cb[0].peer_bda == peer_addr)
+          if(btif_av_cb[0].peer_bda != peer_addr)
           {
               BTIF_TRACE_WARNING(" %s Disconnecting AVRCP ", __FUNCTION__);
               BTA_AvCloseRc(btif_rc_get_connected_peer_handle(peer_addr));
