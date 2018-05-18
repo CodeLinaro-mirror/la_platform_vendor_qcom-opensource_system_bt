bec463e652900e0b943474a79949f807916698fb
Yanfei Zhou <zyanfei@codeaurora.org>
2018-05-18 16:56:40 +0800

    AVRCP_CT: Add support to handle event UIDS_CHANGE.
    Change-Id: Ibe8ca23263a834ce255246777808274118d358da

--- include/hardware/bt_rc.h
+++ include/hardware/bt_rc.h
@@ -618,6 +618,8 @@ typedef void (*btrc_ctrl_addressed_player_changed_callback)(
 typedef void (*btrc_ctrl_now_playing_contents_changed_callback)(
     const RawAddress& bd_addr);
 
+typedef void (* btrc_ctrl_uids_changed_callback)(const RawAddress& bd_addr,
+                                                 uint16_t uid_counter);
 /** BT-RC Controller callback structure. */
 typedef struct {
   /** set to sizeof(BtRcCallbacks) */
@@ -643,6 +645,7 @@ typedef struct {
   btrc_ctrl_addressed_player_changed_callback addressed_player_changed_cb;
   btrc_ctrl_now_playing_contents_changed_callback
       now_playing_contents_changed_cb;
+  btrc_ctrl_uids_changed_callback uids_changed_cb;
 } btrc_ctrl_callbacks_t;
 
 /** Represents the standard BT-RC AVRCP Controller interface. */
@@ -688,7 +691,7 @@ typedef struct {
                                      uint32_t end);
 
   /** change the folder path */
-  bt_status_t (*change_folder_path_cmd)(const RawAddress& bd_addr,
+  bt_status_t (*change_folder_path_cmd)(const RawAddress& bd_addr, uint16_t uid_counter,
                                         uint8_t direction, uint8_t* uid);
 
   /** set browsed player */
