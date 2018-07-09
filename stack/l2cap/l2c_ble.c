GrapheneOS/platform_packages_modules_Bluetooth

DO NOT MERGE: Add bounds check to l2cble_process_sig_cmd L2CAP_CMD_DISC_REQ
Bug: 74121659
Test: Compiles
Change-Id: Ib29dd50cee9decda2d73bb79b84215ea4c6ead75

system/stack/l2cap/l2c_ble.c
@@ -32,6 +32,7 @@
 #include "hcimsgs.h"
 #include "device/include/controller.h"
 #include "stack_config.h"
+#include "log/log.h"
 
 #if (BLE_INCLUDED == TRUE)
 
@@ -829,6 +830,10 @@ void l2cble_process_sig_cmd (tL2C_LCB *p_lcb, UINT8 *p, UINT16 pkt_len)
             break;
 
         case L2CAP_CMD_DISC_REQ:
+            if (p + 4 >= p_pkt_end) {
+              android_errorWriteLog(0x534e4554, "63146237");
+              return;
+            }
             STREAM_TO_UINT16 (lcid, p);
             STREAM_TO_UINT16 (rcid, p);
 

CodeLinaro-mirror/la_platform_vendor_qcom-opensource_system_bt

(cherry picked from commit a75ccdc7ee6c6f60baaf78717926faa9504f9f3f)

stack/l2cap/l2c_ble.c
@@ -33,7 +33,7 @@
 #include "device/include/controller.h"
 #include "device/include/interop.h"
 #include "stack_config.h"
-
+#include "cutils/log.h"
 #if (BLE_INCLUDED == TRUE)
 
 extern fixed_queue_t *btu_general_alarm_queue;
@@ -841,6 +841,10 @@ void l2cble_process_sig_cmd (tL2C_LCB *p_lcb, UINT8 *p, UINT16 pkt_len)
             break;
 
         case L2CAP_CMD_DISC_REQ:
+            if (p + 4 >= p_pkt_end) {
+              android_errorWriteLog(0x534e4554, "63146237");
+              return;
+            }
             STREAM_TO_UINT16 (lcid, p);
             STREAM_TO_UINT16 (rcid, p);
 