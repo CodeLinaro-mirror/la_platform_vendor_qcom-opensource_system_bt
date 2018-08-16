6c59a326f8297095d6d35e5028a6f89ebe691805
zhenchao <zhenchao@codeaurora.org>
2018-08-16 16:44:00 +0800

    A2dp: fix for missing ACK for A2dp pending cmd
    In corner case, audio start stream and bthost ipc in pending state to
    wait ACK for start command, but remote device suspend immediatedly so
    that btif a2dp audio on suspened before offload start complete, then
    a2dp_cmd_pending is reset to NONE. So the following offload start event
    can not send ACK to bthost ipc.
    
    CRs-Fixed: 2299339
    Change-Id: If2e9771f340dd5854a5ed416dfe5f9cdd5be7339
    Signed-off-by: Ashok Kundurthi <askund@codeaurora.org>

--- btif/src/btif_a2dp_audio_interface.cc
+++ btif/src/btif_a2dp_audio_interface.cc
@@ -345,10 +345,9 @@ void btif_a2dp_audio_on_started(tBTA_AV_STATUS status)
             LOG_INFO(LOG_TAG,"calling method a2dp_on_started");
             auto ret = btAudio->a2dp_on_started(mapToStatus(status));
             if (!ret.isOk()) LOG_ERROR(LOG_TAG,"server died");
+            a2dp_cmd_pending = A2DP_CTRL_CMD_NONE;
           }
         }
-        /* clear pending and queued*/
-        a2dp_cmd_pending = A2DP_CTRL_CMD_NONE;
       }
     } else {
       if (a2dp_cmd_pending == A2DP_CTRL_CMD_START) {
@@ -413,10 +412,9 @@ void btif_a2dp_audio_on_suspended(tBTA_AV_STATUS status)
             LOG_INFO(LOG_TAG,"calling method a2dp_on_suspended");
             auto ret = btAudio->a2dp_on_suspended(mapToStatus(status));
             if (!ret.isOk()) LOG_ERROR(LOG_TAG,"server died");
+            a2dp_cmd_pending = A2DP_CTRL_CMD_NONE;
           }
         }
-        /* clear pending and queued*/
-        a2dp_cmd_pending = A2DP_CTRL_CMD_NONE;
       }
     } else {
       if (a2dp_cmd_pending == A2DP_CTRL_CMD_SUSPEND || a2dp_cmd_pending == A2DP_CTRL_CMD_STOP) {
@@ -496,10 +494,9 @@ void btif_a2dp_audio_on_stopped(tBTA_AV_STATUS status)
               LOG_INFO(LOG_TAG, "Remote disconnected when start under progress");
               auto ret = btAudio->a2dp_on_started(mapToStatus(A2DP_CTRL_ACK_DISCONNECT_IN_PROGRESS));
               if (!ret.isOk()) LOG_ERROR(LOG_TAG,"a2dp_on_started: server died");
+              a2dp_cmd_pending = A2DP_CTRL_CMD_NONE;
             }
           }
-          /* clear pending and queued*/
-          a2dp_cmd_pending = A2DP_CTRL_CMD_NONE;
         }
       } else {
         if (a2dp_cmd_pending == A2DP_CTRL_CMD_STOP || a2dp_cmd_pending == A2DP_CTRL_CMD_SUSPEND) {
