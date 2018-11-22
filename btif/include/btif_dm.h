5d7144742fc07adaa994e19a6c5c57bf6f3c2d7f
Harish Bandi <c-hbandi@codeaurora.org>
2018-11-22 10:55:06 +0530

    Bluetooth: Add SSR cleanup support in stack and App(1/3)
    - Notify the BTApp on detecting SSR cases. Notificaiton
      will be done using a callback called ssr cleanup.
    - SSR cases includes command timeout, HW error event.
    - It is the responsibility of application to do it's own
      cleanup followed by killing the process.
    
    Change-Id: I6f42d7655f9f32e19fbfbbb51ffae7717c226fa4

--- btif/include/btif_dm.h
+++ btif/include/btif_dm.h
@@ -22,6 +22,9 @@
 #include "bta_api.h"
 #include "bte_appl.h"
 #include "btif_uid.h"
+#if (defined(SSR_CLEANUP) && SSR_CLEANUP == TRUE)
+#include <hardware/vendor.h>
+#endif
 
 /*******************************************************************************
  *  Functions
@@ -95,6 +98,10 @@ typedef struct {
 #define BTIF_DM_LE_LOCAL_KEY_IRK (1 << 1)
 #define BTIF_DM_LE_LOCAL_KEY_DHK (1 << 2)
 #define BTIF_DM_LE_LOCAL_KEY_ER (1 << 3)
+
+#if (defined(SSR_CLEANUP) && SSR_CLEANUP == TRUE)
+extern btvendor_callbacks_t *bt_vendor_callbacks;
+#endif
 
 void btif_dm_load_ble_local_keys(void);
 void btif_dm_get_ble_local_keys(tBTA_DM_BLE_LOCAL_KEY_MASK* p_key_mask,
