c2acb905c5386477e55fded906bdc10928e91435
Yanfei Zhou <zyanfei@codeaurora.org>
2019-01-15 13:48:19 +0800

    A2DP_SINK: Enable A2DP(SNK) delay report
    Change-Id: I2597a90ac670516934365bbf6e24b58472554803
    Signed-off-by: Yanfei Zhou <zyanfei@codeaurora.org>

--- internal_include/bt_target.h
+++ internal_include/bt_target.h
@@ -1234,7 +1234,7 @@
  *****************************************************************************/
 /* Support delay repoting in Sink role */
 #ifndef A2DP_SINK_DELAY_REPORT
-#define A2DP_SINK_DELAY_REPORT FALSE
+#define A2DP_SINK_DELAY_REPORT TRUE
 #endif
 
 
