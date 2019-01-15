2ab3c0d54a9fce096610bdd2f2a353c9bc502118
Yanfei Zhou <zyanfei@codeaurora.org>
2019-01-15 13:51:34 +0800

    GAP: Refine CoD for Automotive
    1.SERVICE_CLASS: 0x26
       * Bit17: Networking
       * Bit18: Rendering
       * Bit21: Audio
    2.MAJOR_CLASS: 0x04
         Audio/Video
    3.MINOR_CLASS: 0x08
         Hands-free Device
    
    Change-Id: Iebf0d2c4b042ba630dfd085e888ab32fbc51cb7e
    Signed-off-by: Yanfei Zhou <zyanfei@codeaurora.org>

--- internal_include/bt_target.h
+++ internal_include/bt_target.h
@@ -333,18 +333,19 @@
 #define BTM_DEFAULT_DISC_INTERVAL 0x0800
 #endif
 
-/* Default class of device
-* {SERVICE_CLASS, MAJOR_CLASS, MINOR_CLASS}
+/*
+* Change CoD for Auto car-kit
 *
-* SERVICE_CLASS:0x5A (Bit17 -Networking,Bit19 - Capturing,Bit20 -Object
-* Transfer,Bit22 -Telephony)
-* MAJOR_CLASS:0x02 - PHONE
-* MINOR_CLASS:0x0C - SMART_PHONE
+* SERVICE_CLASS:0x26 (Bit17 - Networking, Bit18 - Rendering, Bit21 - Audio)
+* MAJOR_CLASS:0x04 - Audio/Video
+* MINOR_CLASS:0X08 - Hands-free Device
 *
+* Detailed information can be found in below link
+* https://www.bluetooth.com/specifications/assigned-numbers/baseband
 */
 #ifndef BTA_DM_COD
 #define BTA_DM_COD \
-  { 0x5A, 0x02, 0x0C }
+  { 0x26, 0x04, 0x08 }
 #endif
 
 /* The number of SCO links. */
