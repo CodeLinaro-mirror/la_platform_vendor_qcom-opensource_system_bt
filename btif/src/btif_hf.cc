368aaafa91609cc4b3b1093373401673e043152c
Song Gao <songg@codeaurora.org>
2019-03-04 17:10:29 +0800

    HF: KW banned function issue
    Change-Id: Id85c3171b45e3e8ff16bcc18ea9eadf124d2d4f0

--- btif/src/btif_hf.cc
+++ btif/src/btif_hf.cc
@@ -1380,7 +1380,7 @@ static bt_status_t clcc_response(int index, bthf_call_direction_t dir,
         snprintf(&ag_res.str[res_strlen], rem_bytes - 5, ",\"%s", dialnum);
         std::stringstream remaining_string;
         remaining_string << "\"," << type;
-        strncat(&ag_res.str[res_strlen], remaining_string.str().c_str(), 5);
+        strlcat(&ag_res.str[res_strlen], remaining_string.str().c_str(), rem_bytes);
       }
     }
     BTA_AgResult(btif_hf_cb[idx].handle, BTA_AG_CLCC_RES, &ag_res);
