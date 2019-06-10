8a79a7bdef149f1ea936151451dc7c2bbd0bdbf5
Harish Bandi <c-hbandi@codeaurora.org>
2019-06-10 14:31:28 +0530

    BT: HOGP: Remove unnecessary kernel data write call.  - As we are not writing any data to Kernel driver    we can remove co_data call from stack.
    Change-Id: I6285c6f9d4dee42aa7060af75d544c23a6f032a4

--- bta/hh/bta_hh_le.cc
+++ bta/hh/bta_hh_le.cc
@@ -1862,9 +1862,9 @@ void bta_hh_le_input_rpt_notify(tBTA_GATTC_NOTIFY* p_data) {
   memcpy(raw_data->data, p_buf, raw_data->len*sizeof(uint8_t));
   (* bta_hh_cb.p_cback)(BTA_HH_SEND_RAW_DATA_EVT, (tBTA_HH *)raw_data);
 
-  bta_hh_co_data((uint8_t)p_dev_cb->hid_handle, p_buf, p_data->len,
-                 p_dev_cb->mode, 0, /* no sub class*/
-                 p_dev_cb->dscp_info.ctry_code, p_dev_cb->addr, app_id);
+  //bta_hh_co_data((uint8_t)p_dev_cb->hid_handle, p_buf, p_data->len,
+    //             p_dev_cb->mode, 0, /* no sub class*/
+      //           p_dev_cb->dscp_info.ctry_code, p_dev_cb->addr, app_id);
 
   if (p_buf != p_data->value) osi_free(p_buf);
   osi_free(raw_data);
