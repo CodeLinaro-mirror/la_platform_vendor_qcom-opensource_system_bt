0845183b3eb5c70d6b54da72895ae7e496798a44
Martin Brabham <optedoblivion@google.com>
2019-06-20 08:59:24 -0700

    JustWorks: Auto-accept only incoming temporary pairing.
    Bug: 110433804
    Bug: 134461862
    Test: Manual; atest net_test_bluetooth
    Change-Id: I4e3f39bc08e9d9493734a21ea29d76e43aeb50c8
    Merged-In: I4e3f39bc08e9d9493734a21ea29d76e43aeb50c8
    (cherry picked from commit 10e15ee4610969b10e7558969fed8ba229d8e5a0)
    Signed-off-by: Xin Wang <xwangw@codeaurora.org>

--- btif/src/btif_dm.c
+++ btif/src/btif_dm.c
@@ -1096,6 +1096,7 @@ static void btif_dm_ssp_cfm_req_evt(tBTA_DM_SP_CFM_REQ *p_ssp_cfm_req)
 {
     bt_bdaddr_t bd_addr;
     bt_bdname_t bd_name;
+    bool is_incoming = !(pairing_cb.state == BT_BOND_STATE_BONDING);
     UINT32 cod;
     int dev_type;
 
@@ -1133,6 +1134,20 @@ static void btif_dm_ssp_cfm_req_evt(tBTA_DM_SP_CFM_REQ *p_ssp_cfm_req)
     btm_set_bond_type_dev(p_ssp_cfm_req->bd_addr, pairing_cb.bond_type);
 
     pairing_cb.is_ssp = TRUE;
+
+    /* If JustWorks auto-accept */
+    if (p_ssp_cfm_req->just_works) {
+        /* Pairing consent for JustWorks NOT needed if:
+         * 1. Incoming temporary pairing is detected
+         */
+        if (is_incoming && pairing_cb.bond_type == BOND_TYPE_TEMPORARY) {
+            BTIF_TRACE_EVENT(
+                "%s: Auto-accept JustWorks pairing for temporary incoming", __func__);
+            btif_dm_ssp_reply(&bd_addr, BT_SSP_VARIANT_CONSENT, true, 0);
+            return;
+        }
+    }
+
 
     cod = devclass2uint(p_ssp_cfm_req->dev_class);
 
