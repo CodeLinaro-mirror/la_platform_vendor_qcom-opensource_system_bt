402f62bfc0db3f2127d593fdfb5d1577c5d986da
Pulong Xie <puloxie@codeaurora.org>
2019-10-11 15:35:03 +0800

    fix btapp crash when disable BT with BR/EDR/BLE HID connected.
    cast the hh_poll_thread_id type to int, thus we could compare it to 0 normally.
    
    Change-Id: I2e34b24b382b2fdea4eea37034ec931d864aa0a4
    Signed-off-by: Pulong Xie <puloxie@codeaurora.org>

--- btif/co/bta_hh_co.c
+++ btif/co/bta_hh_co.c
@@ -403,7 +403,7 @@ static inline void btif_hh_close_poll_thread(btif_hh_device_t *p_dev)
 {
     APPL_TRACE_DEBUG("%s", __FUNCTION__);
     p_dev->hh_keep_polling = 0;
-    if(p_dev->hh_poll_thread_id > 0)
+    if((int)(p_dev->hh_poll_thread_id) > 0)
         pthread_join(p_dev->hh_poll_thread_id,NULL);
 
     return;
