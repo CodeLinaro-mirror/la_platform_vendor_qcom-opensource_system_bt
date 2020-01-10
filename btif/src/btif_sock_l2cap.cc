731faeaf4b14c0347809198ad1c0c30ba329c157
Song Gao <songg@codeaurora.org>
2020-01-10 18:48:43 +0800

    SOCK_L2CAP: Check sock handle in on_l2cap_close()
    In current implementation, initiate a new sock l2cap connection
    while the previous sock l2cap with the same PSM is still in
    disconnecting might cause crash due to the resource of the new
    sock l2cap is wrongly freed.
    
    The solution is checking sock handle in on_l2cap_close().
    
    Change-Id: Ia99cc544878995020002b52db142e302d7a823d7

--- btif/src/btif_sock_l2cap.cc
+++ btif/src/btif_sock_l2cap.cc
@@ -595,7 +595,7 @@ static void on_l2cap_close(tBTA_JV_L2CAP_CLOSE* p_close, uint32_t id) {
 
   std::unique_lock<std::mutex> lock(state_lock);
   sock = btsock_l2cap_find_by_id_l(id);
-  if (!sock) return;
+  if (!sock || (sock && (sock->handle != (int)p_close->handle))) return;
 
   APPL_TRACE_DEBUG("on_l2cap_close, slot id:%d, fd:%d, %s:%d, server:%d",
                    sock->id, sock->our_fd,
