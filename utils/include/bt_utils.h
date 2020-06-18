912c46cca582f199c84fd03d2653bd1e78824179
YH Guo <yanhuig@codeaurora.org>
2020-06-18 22:29:21 +0800

    bt: support HASTINGS SOC for QRB5165.
    QRB5165 need to support HASTINGS SOC, add the essential code for it.
    CRs-Fixed: 2714047
    
    Change-Id: Icc83ba7eeceb9d3ce2c2539452fb338029432588

--- utils/include/bt_utils.h
+++ utils/include/bt_utils.h
@@ -57,6 +57,8 @@ typedef enum {
     BT_SOC_AR3K,
     BT_SOC_ROME,
     BT_SOC_CHEROKEE,
+    BT_SOC_NAPIER,
+    BT_SOC_HASTINGS,
     /* Add chipset type here */
     BT_SOC_RESERVED
 } bt_soc_type;
