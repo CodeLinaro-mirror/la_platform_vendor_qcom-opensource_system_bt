dd37d111e99ea52dd36d00caa4338686e82de8c9
Rahul Sabnis <rahulsabnis@google.com>
2020-06-26 10:27:07 -0700

    Shows a consent dialog on the local device when pairing a bluetooth low energy device if the local device has a display
    Tag: #security
    Bug: 157038281
    Test: Manual
    Merged-In: I7de396230beb84bd0fa2b0cea346523b6824472a
    Change-Id: I7de396230beb84bd0fa2b0cea346523b6824472a
    (cherry picked from commit b5c0bfc132b296853ba1143f34394e2719ff876d)
    
    Change-Id: Idaa88c7ada594e53522cc13d0dafd72b9f62f89f

--- bta/include/bta_api.h
+++ bta/include/bta_api.h
@@ -395,12 +395,16 @@ typedef uint8_t tBTA_SIG_STRENGTH_MASK;
                                     */
 #define BTA_DM_ENER_INFO_READ 28 /* Energy info read */
 #define BTA_DM_BLE_SC_OOB_REQ_EVT 29 /* SMP SC OOB request event */
+<<<<<<< 9e27012cfa95a5fce00732c0b77cc1eeddf4f96a
 #define BTA_DM_SOC_LOGGING_EVT 31 /* SOC LOGGING event. */
 #define BTA_DM_PKT_TYPE_CHG_EVT 30 /* PACKET TYPE Change event. */
 #define BTA_DM_REM_NAME_EVT 32 /* Remote name event */
 #define BTA_DM_IOT_INFO_EVT 33 /* IOT device info */
 #define BTA_DM_SSR_EVT 34 /* SSR event */
 
+=======
+#define BTA_DM_BLE_CONSENT_REQ_EVT 30 /* SMP consent request event */
+>>>>>>> Shows a consent dialog on the local device when pairing a bluetooth low energy device if the local device has a display.
 typedef uint8_t tBTA_DM_SEC_EVT;
 
 /* Structure associated with BTA_DM_ENABLE_EVT */
