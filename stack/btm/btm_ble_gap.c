792c030931bf4ec096c4bd2482cdb069bd114c88
Balakrishna Godavarthi <bgodavar@codeaurora.org>
2020-07-08 23:00:58 +0530

    GAP: Enable duplicate filtering for BLE scan
    This patch enables duplicate filtering for BLE scan.
    
    Change-Id: Id1ae2fce4941e7d417710e7e97a9dd0c9ddadb56

--- stack/btm/btm_ble_gap.c
+++ stack/btm/btm_ble_gap.c
@@ -398,7 +398,7 @@ tBTM_STATUS BTM_BleObserve(BOOLEAN start, UINT8 duration,
                                                   BTM_BLE_DEFAULT_SFP);
             }
 
-            p_inq->scan_duplicate_filter = BTM_BLE_DUPLICATE_DISABLE;
+            p_inq->scan_duplicate_filter = BTM_BLE_DUPLICATE_ENABLE;
             status = btm_ble_start_scan();
         }
 
