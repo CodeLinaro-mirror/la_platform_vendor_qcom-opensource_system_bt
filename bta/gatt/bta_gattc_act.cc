48a3620f54cd18c8abf6d0945013f5b6a0a9837a
Subramanian Srinivasan <subrsrin@codeaurora.org>
2020-09-24 16:38:52 -0700

    Fix KW memory leak issue for native access socket data
    Fix KW memory leak issue for dynamically allocated memory
    for processing data for native access socket.
    
    Change-Id: I25e3f5de282b779ed6d7bbac87208ddafb39488f

--- bta/gatt/bta_gattc_act.cc
+++ bta/gatt/bta_gattc_act.cc
@@ -1319,6 +1319,7 @@ bool bta_gattc_write_to_socket(tGATT_CL_COMPLETE* p_data, Uuid char_uuid) {
         bta_gattc_cb.is_gatt_skt_connected = false;
         close(bta_gattc_cb.gatt_skt_fd);
         bta_gattc_cb.gatt_skt_fd = -1;
+        delete[] p_skt_data;
         return false;
       }
     }
@@ -1326,6 +1327,7 @@ bool bta_gattc_write_to_socket(tGATT_CL_COMPLETE* p_data, Uuid char_uuid) {
     p_skt_data = (uint8_t*)p_skt_data + sent;
   }
 
+  delete[] p_skt_data;
   return true;
 }
 
