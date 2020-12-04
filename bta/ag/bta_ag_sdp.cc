c64de1785fb6ded0c84e1f285eab1a2018301e6c
Myles Watson <mylesgw@google.com>
2020-12-03 18:54:14 -0800

    SDP: Only start discovery once
    Bug: 174052148
    Test: pair with headphones
    Tag: #security
    Change-Id: I1d014a7b793bb1b66e26652f6696499ea36a6510
    (cherry picked from commit cfa5a74ea90a09e1c7413a25f04332ee2d1e3f21)
    Merged-In: I1d014a7b793bb1b66e26652f6696499ea36a6510
    (cherry picked from commit f0994f6e4723eddaa617b68139f064d945d9389e)

--- bta/ag/bta_ag_sdp.cc
+++ bta/ag/bta_ag_sdp.cc
@@ -33,6 +33,7 @@
 #include "bta_api.h"
 #include "bta_sys.h"
 #include "btm_api.h"
+#include "osi/include/log.h"
 #include "osi/include/osi.h"
 #include "sdp_api.h"
 #include "utl.h"
@@ -433,6 +434,12 @@ void bta_ag_do_disc(tBTA_AG_SCB* p_scb, tBTA_SERVICE_MASK service) {
       uuid_list[1].uu.uuid16 = UUID_SERVCLASS_HEADSET_HS;
       num_uuid = 2;
     }
+  }
+
+  if (p_scb->p_disc_db != nullptr) {
+    android_errorWriteLog(0x534e4554, "174052148");
+    APPL_TRACE_ERROR("Discovery already in progress... returning.");
+    return;
   }
 
   /* allocate buffer for sdp database */
