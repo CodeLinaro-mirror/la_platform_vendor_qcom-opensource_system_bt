6dcaa46a7627dafe0ea0655fe638826ec5480257
Yanfei Zhou <zyanfei@codeaurora.org>
2021-06-16 10:22:54 +0800

    HF_CLIENT: Add null pointer protection for getting BTIF-HF control block
    Change-Id: I3628a0ba9c9f08b0a51d8eac1a9e9aad65bb111e

--- btif/src/btif_hf_client.cc
+++ btif/src/btif_hf_client.cc
@@ -833,6 +833,10 @@ static void btif_hf_client_upstreams_evt(uint16_t event, char* p_param) {
     BTIF_TRACE_DEBUG("%s: event BTA_HF_CLIENT_OPEN_EVT allocating block",
                      __func__);
     cb = btif_hf_client_allocate_cb();
+    if (cb == NULL) {
+      BTIF_TRACE_ERROR("%s: could not allocate block!", __func__);
+      return;
+    }
     cb->handle = p_data->open.handle;
     cb->peer_bda = p_data->open.bd_addr;
   } else if (cb == NULL) {
