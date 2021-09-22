7b0f7c9e4c1c9084de3954eb6b45826d5b55f568
Raghavender Reddy Bujala <c-bujalr@codeaurora.org>
2021-09-22 10:46:55 +0530

    BT: Report AVDT actual MTU as stream MTU to application.
    - AVDT actual MTU size should report to application as
    stream MTU without deducting AVDT header size from it.
    
    Change-Id: I22bd27d76da4ef98d0bce11d277d91d3b81c76db

--- bta/av/bta_av_aact.cc
+++ bta/av/bta_av_aact.cc
@@ -1622,15 +1622,8 @@ void bta_av_str_opened(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
   p_scb->offload_start_pending = false;
   p_scb->suspend_local_sent = FALSE;
 
-  p_scb->stream_mtu =
-      p_data->str_msg.msg.open_ind.peer_mtu - AVDT_MEDIA_HDR_SIZE;
-  if(p_scb->p_cos->cp_is_active()){
-    //peermtu - 14( 13 bytesfor stack headers - 1 byte for cp)
-    mtu_config.mtu = p_scb->stream_mtu - 2;
-  } else {
-    //peermtu - 13 bytes (for stack headers)
-    mtu_config.mtu = p_scb->stream_mtu - 1;
-  }
+  p_scb->stream_mtu = p_data->str_msg.msg.open_ind.peer_mtu - AVDT_MEDIA_HDR_SIZE;
+  mtu_config.mtu = p_data->str_msg.msg.open_ind.peer_mtu;
   mtu_config.hndl = p_scb->hndl;
   mtu = bta_av_chk_mtu(p_scb, p_scb->stream_mtu);
   APPL_TRACE_DEBUG("%s: l2c_cid: 0x%x stream_mtu: %d mtu: %d", __func__,
