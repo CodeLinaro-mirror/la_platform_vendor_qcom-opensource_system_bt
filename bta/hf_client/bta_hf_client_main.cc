ddaae27868cc6311c5b37eaf1930f31c7f738955
ManiSrinivas Srungavarapu <msrungav@codeaurora.org>
2021-10-25 12:03:09 +0530

    Modified to code to clean up hfp client connection collision and connection close sequence
    Change-Id: I05b480504f112e3056fd476eb472681e84246db3
    (cherry picked from commit a88617260d532b2590a1a47e408055fb2ec7efcb)

--- bta/hf_client/bta_hf_client_main.cc
+++ bta/hf_client/bta_hf_client_main.cc
@@ -329,6 +329,8 @@ void bta_hf_client_resume_open(tBTA_HF_CLIENT_CB* client_cb) {
     client_cb->state = BTA_HF_CLIENT_OPENING_ST;
     tBTA_HF_CLIENT_DATA msg;
     msg.hdr.layer_specific = client_cb->handle;
+    msg.api_open.bd_addr = client_cb->peer_addr;
+    msg.api_open.sec_mask = client_cb->cli_sec_mask;
     bta_hf_client_start_open(&msg);
   }
 }
@@ -386,7 +388,7 @@ void bta_hf_client_collision_cback(UNUSED_ATTR tBTA_SYS_CONN_STATUS status,
 
     /* reopen registered server */
     /* Collision may be detected before or after we close servers. */
-    // bta_hf_client_start_server();
+    bta_hf_client_start_server();
 
     /* Start timer to handle connection opening restart */
     alarm_set_on_mloop(client_cb->collision_timer,
