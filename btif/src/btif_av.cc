9ca783675a1e733e5c9e64924bc32b14326552af
Fei Zheng <quic_zfei@quicinc.com>
2022-02-08 17:14:18 +0800

    A2DP_SINK: Fix cannot update decoder when reconfigure codec
    Issue:
    When connect some phone and the phone change the codec, the codec configuration
    cannot be saved and updated to decoder
    
    Root cause:
    To change the codec, the phone will close stream and set configuration.
    set configuration may be received before finish handle the close procedure, and
    ed to miss the configuration
    
    Fix:
    Save the configuration and only clear in A2DP sink clean up
    
    Change-Id: Ie962a83366ec8c98b4aea31714a38c5e19801066

--- btif/src/btif_av.cc
+++ btif/src/btif_av.cc
@@ -589,6 +589,9 @@ class BtifAvSink {
   BtifAvPeer* FindOrCreatePeer(const RawAddress& peer_address,
                                tBTA_AV_HNDL bta_handle);
 
+  uint8_t* FindCodec(const RawAddress& peer_address);
+  void ClearCodec(const RawAddress& peer_address);
+  void SaveCodec(const RawAddress& peer_address, uint8_t* codec, int size);
   /**
    * Check whether a connection to a peer is allowed.
    * The check considers the maximum number of connected peers.
@@ -677,6 +680,7 @@ class BtifAvSink {
 
  private:
   void CleanupAllPeers();
+  void CleanupAllCodecs();
 
   btav_sink_callbacks_t* callbacks_;
   bool enabled_;
@@ -685,6 +689,9 @@ class BtifAvSink {
   RawAddress active_peer_;
   std::map<uint8_t, tBTA_AV_HNDL> peer_id2bta_handle_;
   std::mutex mutex_;
+  // Save the codecs for devices to work around some phone no sound issue when change codec
+  // the codec will be cleaned in btif_av_sink clean up
+  std::map<RawAddress, uint8_t*>map_codec_;
 };
 
 /*****************************************************************************
@@ -1000,11 +1007,14 @@ bool BtifAvPeer::IsStreaming() const {
 }
 
 void BtifAvPeer::SaveAvConfig(uint8_t* codec, int size) {
+  LOG_INFO(LOG_TAG, "%s: codec=%p, size=%d", __func__, codec, size);
+
   if (codec_ != nullptr) {
     osi_free(codec_);
   }
   codec_ = (uint8_t *)osi_calloc(size);
   memcpy(codec_, codec, size);
+  btif_av_sink.SaveCodec(PeerAddress(), codec, size);
 }
 
 BtifAvSource::~BtifAvSource() { CleanupAllPeers(); }
@@ -1247,6 +1257,7 @@ void BtifAvSink::Cleanup() {
 
   btif_disable_service(BTA_A2DP_SINK_SERVICE_ID);
   CleanupAllPeers();
+  CleanupAllCodecs();
 
   callbacks_ = nullptr;
   enabled_ = false;
@@ -1318,6 +1329,29 @@ BtifAvPeer* BtifAvSink::FindOrCreatePeer(const RawAddress& peer_address,
   return peer;
 }
 
+uint8_t* BtifAvSink::FindCodec(const RawAddress& peer_address) {
+  auto it = map_codec_.find(peer_address);
+  if (it != map_codec_.end()) return it->second;
+  return nullptr;
+}
+
+void BtifAvSink::ClearCodec(const RawAddress& peer_address) {
+  auto it = map_codec_.find(peer_address);
+  if (it != map_codec_.end()) {
+    if (it->second != nullptr) {
+      osi_free(it->second);
+    }
+    map_codec_.erase(it);
+  }
+}
+
+void BtifAvSink::SaveCodec(const RawAddress& peer_address, uint8_t* codec, int size) {
+  ClearCodec(peer_address);
+  uint8_t* saved_codec = (uint8_t* )osi_malloc(size);
+  memcpy(saved_codec, codec, size);
+  map_codec_.insert(std::make_pair(peer_address, saved_codec));
+}
+
 bool BtifAvSink::AllowedToConnect(const RawAddress& peer_address) const {
   int connected = 0;
 
@@ -1370,6 +1404,17 @@ void BtifAvSink::CleanupAllPeers() {
     peer->Cleanup();
     peers_.erase(it);
     delete peer;
+  }
+}
+
+void BtifAvSink::CleanupAllCodecs() {
+  while (!map_codec_.empty()) {
+    auto it = map_codec_.begin();
+    uint8_t* codec = it->second;
+    if (codec != nullptr) {
+      osi_free(codec);
+    }
+    map_codec_.erase(it);
   }
 }
 
@@ -2955,8 +3000,17 @@ static void set_active_src_int(const RawAddress& peer_address) {
       BTIF_TRACE_DEBUG("%s: %s update decoder", __PRETTY_FUNCTION__,
                        peer_address.ToString().c_str());
       // Update the codec info of the A2DP Sink decoder
-      btif_a2dp_sink_update_decoder((uint8_t*)(peer->GetAvConfig()));
-      result = true;
+      uint8_t* codec = peer->GetAvConfig();
+      if (codec == nullptr) {
+        BTIF_TRACE_ERROR("%s: peer->GetAvConfig() is null, try to get saved codec", __func__);
+        codec = btif_av_sink.FindCodec(peer_address);
+      }
+      if (codec != nullptr) {
+        btif_a2dp_sink_update_decoder(codec);
+        result = true;
+      } else {
+        BTIF_TRACE_ERROR("%s: faild to update decoder", __func__);
+      }
     } else {
       BTIF_TRACE_ERROR("%s: Error setting %s as active Source peer", __func__,
                        peer_address.ToString().c_str());
