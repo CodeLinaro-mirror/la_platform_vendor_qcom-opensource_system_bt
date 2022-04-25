fcb480b68bb81c36f8bbc1e1508668b5a9cb688e
Satish Kumar Kodishala <quic_skodisha@quicinc.com>
2022-04-25 14:41:22 +0530

    Fall back to CLOCK_BOOTTIME if CLOCK_BOOTTIME_ALARM fails
    During wakeup timer creation, fall back to CLOCK_BOOTTIME
    if CLOCK_BOOTTIME_ALARM fails.
    
    CRs-Fixed: 3181927
    Change-Id: I457da84386e461774734ebd3d3486a14248bfeef

--- osi/src/alarm.cc
+++ osi/src/alarm.cc
@@ -116,12 +116,6 @@ struct alarm_t {
 int64_t TIMER_INTERVAL_FOR_WAKELOCK_IN_MS = 3000;
 static const clockid_t CLOCK_ID = CLOCK_BOOTTIME;
 
-#if (KERNEL_MISSING_CLOCK_BOOTTIME_ALARM == TRUE)
-static const clockid_t CLOCK_ID_ALARM = CLOCK_BOOTTIME;
-#else
-static const clockid_t CLOCK_ID_ALARM = CLOCK_BOOTTIME_ALARM;
-#endif
-
 // This mutex ensures that the |alarm_set|, |alarm_cancel|, and alarm callback
 // functions execute serially and not concurrently. As a result, this mutex
 // also protects the |alarms| list.
@@ -328,7 +322,16 @@ static bool lazy_initialize(void) {
   if (!timer_create_internal(CLOCK_ID, &timer)) goto error;
   timer_initialized = true;
 
-  if (!timer_create_internal(CLOCK_ID_ALARM, &wakeup_timer)) goto error;
+  if (!timer_create_internal(CLOCK_BOOTTIME_ALARM, &wakeup_timer)) {
+    LOG_ERROR(LOG_TAG, "%s wakeup timer creation failed with CLOCK_BOOTTIME_ALARM",
+                    __func__);
+    if (!timer_create_internal(CLOCK_BOOTTIME, &wakeup_timer)) {
+      LOG_ERROR(LOG_TAG, "%s wakeup timer creation failed with CLOCK_BOOTTIME",
+                    __func__);
+      goto error;
+    }
+  }
+
   wakeup_timer_initialized = true;
 
   alarm_expired = semaphore_new(0);
