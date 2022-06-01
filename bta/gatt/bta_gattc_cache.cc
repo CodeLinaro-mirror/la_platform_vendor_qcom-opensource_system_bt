da2a5b1ebf2b54b94127e96dfb9b1457480ca831
Naga Bhavani Akella <quic_nakella@quicinc.com>
2022-06-01 11:08:54 +0530

    lpm_srvc_disc: resolved crash caused while reading services from data base.
     -- Crash is caused in few corner cases while reading contents from
    gatt data base. Since the same pointer is used to retrieve
    db info in frameworks as well, calling the api to update gatt db in lpm profile
    after callback to frameworks.
     -- Removing btif_lpm_gattc_inform_notify_reg call from stack as it is
    no longer needed.
    
    Change-Id: I9873c5a917fcec4f75e43e2b7a035ed92910326c

--- bta/gatt/bta_gattc_cache.cc
+++ bta/gatt/bta_gattc_cache.cc
@@ -46,9 +46,6 @@
 #include "sdp_api.h"
 #include "sdpdefs.h"
 #include "utl.h"
-#ifdef BT_LPM_SUPPORTED
-#include "stack/lpm/lpm_int.h"
-#endif
 
 using base::StringPrintf;
 using bluetooth::Uuid;
@@ -713,10 +710,6 @@ void bta_gattc_get_gatt_db(uint16_t conn_id, uint16_t start_handle,
 
   bta_gattc_get_gatt_db_impl(p_clcb->p_srcb, start_handle, end_handle, db,
                              count);
-#ifdef BT_LPM_SUPPORTED
-  VLOG(1) << "service discovery done inform lpm profile";
-  bta_rmt_srvc_disc_done(conn_id, *db, *count);
-#endif /*BT_LPM_SUPPORTED*/
 
 }
 
