455a1c120c425b082338caa461baf0c2954eacb0
Amit Kumar Prasad <quic_amitpras@quicinc.com>
2022-12-22 12:54:29 +0530

    Disable AVRCP CT 1.6 optional features
     -Disable CoverArt support
     -Disable Avrcp CT browsing Support
    
    CRs-Fixed: 3373377
    Change-Id: I59b4535406c68d368ec789eb9fafd17dd01184cc

--- bta/av/bta_av_main.cc
+++ bta/av/bta_av_main.cc
@@ -811,18 +811,10 @@ static void bta_av_api_register(tBTA_AV_DATA* p_data) {
            */
           if ((profile_initialized == UUID_SERVCLASS_AUDIO_SOURCE) ||
               (profile_initialized == UUID_SERVCLASS_AUDIO_SINK)) {
-            if (strcmp(board_prop, "neo") == 0) {
-              APPL_TRACE_DEBUG("Create SDP for neo with Avrcp 1.4");
-              bta_ar_reg_avrc(UUID_SERVCLASS_AV_REMOTE_CONTROL, NULL, NULL,
-                              p_bta_av_cfg->avrc_ct_cat, BTA_ID_AV,
-                              (bta_av_cb.features & BTA_AV_FEAT_BROWSE),
-                              AVRC_REV_1_4);
-            } else {
-              bta_ar_reg_avrc(UUID_SERVCLASS_AV_REMOTE_CONTROL, NULL, NULL,
-                              p_bta_av_cfg->avrc_ct_cat, BTA_ID_AV,
-                              (bta_av_cb.features & BTA_AV_FEAT_BROWSE),
-                              AVRC_REV_1_6);
-            }
+            bta_ar_reg_avrc(UUID_SERVCLASS_AV_REMOTE_CONTROL, NULL, NULL,
+                            p_bta_av_cfg->avrc_ct_cat, BTA_ID_AV,
+                            (bta_av_cb.features & BTA_AV_FEAT_BROWSE),
+                            AVRC_REV_1_6);
           }
 #endif
         }
