059bbde5a4e9270179f98cf7db21e8dc1897fa11
Cheng Jiang <quic_chejiang@quicinc.com>
2023-03-28 10:24:54 +0800

    Fix the segment fault error during OPP
    alarm may be removed after post to libchrome task and before the task excute which will cause the crash
    
    Change-Id: I95566bf8fe28b512cf54915915c6e804a0b7bdd0
    Signed-off-by: Cheng Jiang <quic_chejiang@quicinc.com>

--- osi/src/alarm.cc
+++ osi/src/alarm.cc
@@ -100,6 +100,7 @@ struct alarm_t {
   period_ms_t prev_deadline;  // Previous deadline - used for accounting of
                               // periodic timers
   bool is_periodic;
+  bool is_posted;
   fixed_queue_t* queue;  // The processing queue to add this alarm to
   alarm_callback_t callback;
   void* data;
@@ -421,7 +422,8 @@ static void remove_pending_alarm(alarm_t* alarm) {
   list_remove(alarms, alarm);
 
   if (alarm->for_msg_loop) {
-    alarm->closure.i.Cancel();
+    if (!alarm->is_posted)
+      alarm->closure.i.Cancel();
   } else {
     while (fixed_queue_try_remove_from_queue(alarm->queue, alarm) != NULL) {
       // Remove all repeated alarm instances from the queue.
@@ -579,6 +581,7 @@ static void alarm_ready_generic(alarm_t* alarm,
   // some of its internal state. This is useful to distinguish between expired
   // alarms and active ones.
   //
+  alarm->is_posted = false;
   alarm_callback_t callback = alarm->callback;
   void* data = alarm->data;
   period_ms_t deadline = alarm->deadline;
@@ -600,7 +603,9 @@ static void alarm_ready_generic(alarm_t* alarm,
 
   lock.unlock();
 
-  callback(data);
+  if (callback) {
+    callback(data);
+  }
 }
 
 static void alarm_ready_mloop(alarm_t* alarm) {
@@ -661,6 +666,7 @@ static void callback_dispatch(UNUSED_ATTR void* context) {
 
       alarm->closure.i.Reset(Bind(alarm_ready_mloop, alarm));
       get_message_loop()->task_runner()->PostTask(FROM_HERE, alarm->closure.i.callback()); // gghai
+      alarm->is_posted = true;
     } else {
       fixed_queue_enqueue(alarm->queue, alarm);
     }
