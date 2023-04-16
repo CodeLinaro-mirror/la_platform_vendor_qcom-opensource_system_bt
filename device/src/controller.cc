05bd169476802e51e1adb8d1a4c0e6c6aa297c6c
Shudan Liu <quic_shudan@quicinc.com>
2023-04-16 12:15:14 +0800

    bt: enable bluetooth for qcs6490
    Change-Id: I868984c44cf868d257cda307a34630f1dc35fc7b

--- device/src/controller.cc
+++ device/src/controller.cc
@@ -112,6 +112,9 @@ void send_soc_log_command(bool value) {
   } else if (soc_type == BT_SOC_CHEROKEE) {
     LOG_INFO(LOG_TAG, "%s for BT_SOC_CHEROKEE.", __func__);
     BTM_VendorSpecificCommand(HCI_VS_HOST_LOG_OPCODE, 2, param_cherokee, NULL);
+  } else if (soc_type == BT_SOC_HASTINGS) {
+    LOG_INFO(LOG_TAG, "%s for BT_SOC_HASTINGS.", __func__);
+    BTM_VendorSpecificCommand(HCI_VS_HOST_LOG_OPCODE, 2, param_cherokee, NULL);
   }
 }
 #ifndef QLOGKIT_USERDEBUG
@@ -330,7 +333,7 @@ static future_t* start_up(void) {
   }
 
   // read scrambling support from controller incase of cherokee
-  if (soc_type == BT_SOC_CHEROKEE) {
+  if (soc_type == BT_SOC_CHEROKEE || soc_type == BT_SOC_HASTINGS) {
     response =
           AWAIT_COMMAND(packet_factory->make_read_scrambling_supported_freqs());
     if(response) {
