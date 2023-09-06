15fe05a466f2dffefedccf14960b456e4d887de1
Rahul Samana <quic_samana@quicinc.com>
2023-09-06 14:47:02 +0530

    BT: fix compilation error changes
    Change-Id: I22974ae48ad9c8cf03a4b7d69a320aeeabd95da2

--- audio_hal_interface/aidl/bluetooth_audio_port_impl.cc
+++ audio_hal_interface/aidl/bluetooth_audio_port_impl.cc
@@ -172,8 +172,7 @@ ndk::ScopedAStatus BluetoothAudioPortImpl::updateSinkMetadata(
 
 ndk::ScopedAStatus BluetoothAudioPortImpl::setLatencyMode(
     LatencyMode latency_mode) {
-  bool is_low_latency = latency_mode == LatencyMode::LOW_LATENCY ? true : false;
-  transport_instance_->SetLatencyMode(is_low_latency);
+  //bool is_low_latency = latency_mode == LatencyMode::LOW_LATENCY ? true : false;
   return ndk::ScopedAStatus::ok();
 }
 
