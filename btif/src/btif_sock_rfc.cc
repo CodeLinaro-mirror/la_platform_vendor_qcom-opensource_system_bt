14a21abf14fe9cc076b2948b7c2627e4eac99764
Amarender Kanchugatla <quic_akanchug@quicinc.com>
2023-10-11 15:11:26 +0530

    BT Single Stack::Code Changes in BT_BTIF
        --> Changing RFCOMM API calls from Ctrl Channel to Data Channel
        --> Fix for RFCOMM Disconnect
    
    Change-Id: I6612a0fb1e6ed885697ac005ccbcaaa47a9ab6c0

--- btif/src/btif_sock_rfc.cc
+++ btif/src/btif_sock_rfc.cc
@@ -771,7 +771,7 @@ bt_status_t btsock_rfc_listen(const char* service_name,
   msgStr.append(protoMsg);
   ALOGI("%s: BT_RFCOMM_CREATE_SOCKET length: %d",__func__, msgStr.length());
 #ifndef SS_STUB_ENABLED
-  gBTSSInterface->postTxMsg(msgStr);
+  gBTSSInterface->postDataChTxMsg(msgStr);
 #else
   gBTSSStubInterface->postTxMsg(msgStr);
 #endif
@@ -865,7 +865,7 @@ bt_status_t btsock_rfc_connect(const RawAddress* bd_addr,
   msgStr.append(protoMsg);
   ALOGI("%s: BT_RFCOMM_CONNECT_SOCKET length: %d",__func__, msgStr.length());
 #ifndef SS_STUB_ENABLED
-  gBTSSInterface->postTxMsg(msgStr);
+  gBTSSInterface->postDataChTxMsg(msgStr);
 #else
   gBTSSStubInterface->postTxMsg(msgStr);
 #endif
@@ -916,7 +916,7 @@ static void free_rfc_slot_scn(rfc_slot_t* slot) {
         msgStr.append(protoMsg);
         ALOGI("%s: BT_RFCOMM_CLOSE_SERVER length: %d and data: %s",__func__, msgStr.length(),msgStr.c_str());
       #ifndef SS_STUB_ENABLED
-        gBTSSInterface->postTxMsg(msgStr);
+        gBTSSInterface->postDataChTxMsg(msgStr);
       #else
         gBTSSStubInterface->postTxMsg(msgStr);
       #endif
@@ -1002,7 +1002,7 @@ static void cleanup_rfc_slot(rfc_slot_t* slot) {
         msgStr.append(protoMsg);
         ALOGI("%s: BT_RFCOMM_DISCONNECT_SOCKET length: %d and data: %s",__func__, msgStr.length(),msgStr.c_str());
       #ifndef SS_STUB_ENABLED
-        gBTSSInterface->postTxMsg(msgStr);
+        gBTSSInterface->postDataChTxMsg(msgStr);
       #else
         gBTSSStubInterface->postTxMsg(msgStr);
       #endif
@@ -1087,6 +1087,7 @@ static void ss_cli_rfc_connect (int fd, const RawAddress* addr, int channel,
   rfc_slot_t* client_rs = find_rfc_slot_by_scn(channel);
   if (!client_rs) return;
   client_rs->mtu = mtu;
+  client_rs->rfc_handle = channel;
   /*if (status != PORT_OPEN_SUCCUESS) {
     LOG_ERROR(LOG_TAG, "%s slate returned failure Status : %d",__func__, status);
     return;
