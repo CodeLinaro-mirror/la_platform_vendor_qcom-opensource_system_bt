24abb129a9812dcd37a128f434f16cf5898f3be0
Brian Delwiche <delwiche@google.com>
2024-05-31 21:46:17 +0000

    Disallow connect with Secure Connections downgrade
    As a guard against the BLUFFS attack, check security parameters of
    incoming connections against cached values and disallow connection if
    these parameters are downgraded or changed from their cached values.
    
    This CL adds the connection-time check for Secure Connections mode.
    
    Bug: 314331379
    Test: m libbluetooth
    Test: manual
    
    To test this CL, please ensure that BR/EDR initial connections and reconnections  (after cycling remote devices, cycling Bluetooth, restarting the phone, etc.) work against remote devices which both support and do not support Secure Connections mode, and with all supported bonding types.  Basic validation of LE bonding functionality should be done as well.
    
    Tag: #security
    Ignore-AOSP-First: Security
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:4da47591b9530849123c238b2747f5304058db2d)
    Merged-In: I7b9dd1a18f1c04df88aabe89d4790fef9112da7e
    Change-Id: I7b9dd1a18f1c04df88aabe89d4790fef9112da7e
    (cherry picked from commit e7f87d23b295d3b3def26061afb3739e1ee71c99)

--- stack/btm/btm_sec.cc
+++ stack/btm/btm_sec.cc
@@ -5123,6 +5123,13 @@ void btm_sec_link_key_notification(const RawAddress& p_bda,
     }
   }
 
+  if (p_dev_rec->is_bond_type_persistent() &&
+      (p_dev_rec->is_device_type_br_edr() ||
+       p_dev_rec->is_device_type_dual_mode())) {
+    btm_sec_store_device_sc_support(p_dev_rec->get_br_edr_hci_handle(),
+                                    p_dev_rec->SupportsSecureConnections());
+  }
+
   /* If name is not known at this point delay calling callback until the name is
    */
   /* resolved. Unless it is a HID Device and we really need to send all link
@@ -6366,6 +6373,15 @@ static bool btm_sec_queue_encrypt_request(const RawAddress& bd_addr,
  ******************************************************************************/
 void btm_sec_set_peer_sec_caps(tACL_CONN* p_acl_cb,
                                tBTM_SEC_DEV_REC* p_dev_rec) {
+  // Drop the connection here if the remote attempts to downgrade from Secure
+  // Connections mode.
+  if (btm_sec_is_device_sc_downgrade(hci_handle, sc_supported)) {
+    acl_set_disconnect_reason(HCI_ERR_HOST_REJECT_SECURITY);
+    btm_sec_send_hci_disconnect(p_dev_rec, HCI_ERR_AUTH_FAILURE, hci_handle);
+    LOG_WARN("Remote attempted to downgrade from Secure Connections mode");
+    return;
+  }
+
   if ((btm_cb.security_mode == BTM_SEC_MODE_SP ||
        btm_cb.security_mode == BTM_SEC_MODE_SP_DEBUG ||
        btm_cb.security_mode == BTM_SEC_MODE_SC) &&
