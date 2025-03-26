3aa63f049ed07381a0b531201fbe5e45229b22d6
Yanfei Zhou <quic_zyanfei@quicinc.com>
2025-03-26 12:14:53 +0530

    L2C: Fix NPE in l2cu_release_ccb
    Add null pointer protection in l2cu_release_ccb
    
    Change-Id: I2497d3027dd3da61e1b1c4ccc2c46f62af609b7d

--- stack/l2cap/l2c_utils.cc
+++ stack/l2cap/l2c_utils.cc
@@ -14,6 +14,10 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
+ *  Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ *
+ *  Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ *  SPDX-License-Identifier: BSD-3-Clause-Clear.
  ******************************************************************************/
 
 /******************************************************************************
@@ -1529,6 +1533,10 @@ bool l2cu_start_post_bond_timer(uint16_t handle) {
  *
  ******************************************************************************/
 void l2cu_release_ccb(tL2C_CCB* p_ccb) {
+  if (!p_ccb) {
+    L2CAP_TRACE_WARNING("%s p_ccb is null, return", __func__);
+    return;
+  }
   tL2C_LCB* p_lcb = p_ccb->p_lcb;
   tL2C_RCB* p_rcb = p_ccb->p_rcb;
 
@@ -1538,8 +1546,10 @@ void l2cu_release_ccb(tL2C_CCB* p_ccb) {
   /* If already released, could be race condition */
   if (!p_ccb->in_use) return;
 
-  btsnoop_get_interface()->clear_l2cap_allowlist(
-      p_lcb->Handle(), p_ccb->local_cid, p_ccb->remote_cid);
+  if (p_lcb) {
+    btsnoop_get_interface()->clear_l2cap_allowlist(
+        p_lcb->Handle(), p_ccb->local_cid, p_ccb->remote_cid);
+  }
 
   if (p_rcb && (p_rcb->psm != p_rcb->real_psm)) {
     BTM_SecClrServiceByPsm(p_rcb->psm);
