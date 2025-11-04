406b8f6f338010bf998fda74fffb2207879f88e4
gaoxiang <quic_gaoxiang@quicinc.com>
2025-11-04 12:26:08 +0800

    RESTRICT AUTOMERGE Contain avrc_ctrl_pars_vendor_cmd OOB write(part 2)
    Bug: 181860042
    Test: net_test_stack
    Tag: #security
    Ignore-AOSP-First: Security
    Change-Id: Ifd68ab0597bf2a5d1ff69bfe0f9125be5c4376ae

--- stack/avrc/avrc_pars_tg.c
+++ stack/avrc/avrc_pars_tg.c
@@ -84,6 +84,11 @@ static tAVRC_STS avrc_ctrl_pars_vendor_cmd(tAVRC_MSG_VENDOR *p_msg, tAVRC_COMMAN
 
         BE_STREAM_TO_UINT8 (p_result->reg_notif.event_id, p);
         BE_STREAM_TO_UINT32 (p_result->reg_notif.param, p);
+
+        if (p_result->reg_notif.event_id == 0 ||
+            p_result->reg_notif.event_id > AVRC_NUM_NOTIF_EVENTS) {
+          status = AVRC_STS_BAD_PARAM;
+        }
         break;
     default:
         status = AVRC_STS_BAD_CMD;
