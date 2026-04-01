45c9e02c4875852f4c58cdac5bf8aa1e4a51c4f0
Sreerag Vaniyam Veettil <sveettil@qti.qualcomm.com>
2026-03-31 21:53:07 -0700

    HFP_AG: Updating profile version. - Profile versions changed from 1.6 to 1.8 for AG role.
    Change-Id: I61876c43c25a7f222130d4bf179c123c13f62081

--- bta/ag/bta_ag_sdp.cc
+++ bta/ag/bta_ag_sdp.cc
@@ -168,7 +168,7 @@ bool bta_ag_add_record(uint16_t service_uuid, char* p_service_name, uint8_t scn,
   /* add profile descriptor list */
   if (service_uuid == UUID_SERVCLASS_AG_HANDSFREE) {
     profile_uuid = UUID_SERVCLASS_HF_HANDSFREE;
-    version = HFP_VERSION_1_6;
+    version = HFP_VERSION_1_8;
   } else {
     profile_uuid = UUID_SERVCLASS_HEADSET;
     version = HSP_VERSION_1_2;
